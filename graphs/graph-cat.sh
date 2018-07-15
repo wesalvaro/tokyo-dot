e0f6a18afd4324973f853ff08d722810884f29e1
Wes Alvaro <git@wesalvaro.com>
2018-07-15 21:49:58 +0900

    Set font on edge labels

--- graphs/graph-cat.sh
+++ graphs/graph-cat.sh
@@ -1,5 +1,5 @@
 echo 'strict digraph {'
-echo '  edge [style=bold color="#000000"]'
+echo '  edge [style=bold color="#000000" fontname=Arial]'
 echo '  graph [sep=10 bgcolor="#666666"]'
 echo '  node [shape=record style="filled,bold" fontname=Arial fillcolor="#ffffff"]'
 cat line-*.dot
