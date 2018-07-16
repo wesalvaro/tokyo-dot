e6b9d13eb5d450cc96904682bccbac705053772b
Wes Alvaro <git@wesalvaro.com>
2018-07-16 16:42:47 +0900

    Minor cleanup

--- renders/go/main.go
+++ renders/go/main.go
@@ -7,24 +7,24 @@ import (
 	"strconv"
 )
 
-func main() {
-	f, err := os.Open("graph.dot")
+func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	f, err := os.Open("graph.dot")
+	checkErr(err)
 	tokyo := readGraph(f)
 	fmt.Println(tokyo)
 	if os.Args[1] == "route" {
 		doRoute(tokyo, os.Args[2:]...)
 	} else if os.Args[1] == "explore" {
 		min, err := strconv.Atoi(os.Args[3])
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkErr(err)
 		limit, err := strconv.Atoi(os.Args[4])
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkErr(err)
 		doExplore(tokyo, os.Args[2], float64(min), float64(limit))
 	}
 }
@@ -34,11 +34,13 @@ func doRoute(g *trainGraph, stations ...string) {
 	var fullRoute route
 	for i := 0; i < len(stations)-1; i++ {
 		route := findRoute(g, stations[i], stations[i+1])
+		var stations []*station
 		if i > 1 {
-			fullRoute.stations = append(fullRoute.stations, route.stations[1:]...)
+			stations = route.stations[1:]
 		} else {
-			fullRoute.stations = append(fullRoute.stations, route.stations...)
+			stations = route.stations
 		}
+		fullRoute.stations = append(stations, stations...)
 		fullRoute.time += route.time
 	}
 	render(g, fullRoute)
