62c4a1a4c9516411264fd782d5eedb6c408778a8
Wes Alvaro <git@wesalvaro.com>
2018-07-31 00:11:35 +0900

    Initial AppEngine app testing

--- renders/go/main.go
+++ renders/go/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"google.golang.org/appengine"
 )
@@ -16,13 +17,44 @@ func checkErr(err error) {
 	}
 }
 
+var tokyo *trainGraph
+
 func main() {
-	http.HandleFunc("/", handle)
+	f, err := os.Open("graph.dot")
+	checkErr(err)
+	tokyo = readGraph(f)
+	http.HandleFunc("/route/", handle)
 	appengine.Main()
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+	w.Header().Set("Content-Type", "text/html")
+	parts := strings.Split(r.URL.String()[1:], "/")
+	numParts := len(parts)
+	fmt.Fprintln(w, `<!DOCTYPE html><meta charset="UTF-8"><h1>Routing</h1>`)
+	if numParts == 0 {
+		fmt.Fprintf(w, "try a command")
+		return
+	} else if parts[0] == "route" {
+		if numParts < 3 {
+			fmt.Fprintf(w, "you need at least two stations")
+			return
+		}
+		route := findMultiRoute(tokyo, parts[1:]...)
+		for _, d := range parts[1 : len(parts)-1] {
+			fmt.Fprintf(w, "%s to ", d)
+		}
+		fmt.Fprint(w, parts[len(parts)-1])
+		fmt.Fprintf(w, "<div>Time: %.0f</div>", route.time)
+		fmt.Fprintf(w, "<ol>")
+		for i := 0; i < len(route.stations)-1; i++ {
+			f := route.stations[i]
+			t := route.stations[i+1]
+			fmt.Fprintf(w, "<li>%s -> %s</li>", f.nameEn, t.nameEn)
+		}
+		fmt.Fprintf(w, "</ol>")
+	}
+	fmt.Fprintln(w)
 }
 
 func cli() {
@@ -31,35 +63,22 @@ func cli() {
 	tokyo := readGraph(f)
 	fmt.Println(tokyo)
 	if os.Args[1] == "route" {
-		doRoute(tokyo, os.Args[2:]...)
+		cliRoute(tokyo, os.Args[2:]...)
 	} else if os.Args[1] == "explore" {
 		min, err := strconv.Atoi(os.Args[3])
 		checkErr(err)
 		limit, err := strconv.Atoi(os.Args[4])
 		checkErr(err)
-		doExplore(tokyo, os.Args[2], float64(min), float64(limit))
+		cliExplore(tokyo, os.Args[2], float64(min), float64(limit))
 	}
 }
 
-func doRoute(g *trainGraph, stations ...string) {
+func cliRoute(g *trainGraph, stations ...string) {
 	fmt.Printf("Routing: %s\n", stations)
-	var fullRoute route
-	for i := 0; i < len(stations)-1; i++ {
-		route := findRoute(g, stations[i], stations[i+1])
-		var stations []*station
-		if i > 1 {
-			stations = route.stations[1:]
-		} else {
-			stations = route.stations
-		}
-		fullRoute.stations = append(stations, stations...)
-		fullRoute.time += route.time
-	}
-	render(g, fullRoute)
+	render(g, findMultiRoute(g, stations...))
 }
 
-func doExplore(g *trainGraph, station string, min, limit float64) {
-	fmt.Printf("Exploring from: %s\n", station)
+func cliExplore(g *trainGraph, station string, min, limit float64) {
 	routes := exploreFrom(g, station, min, limit)
 	for dest, route := range routes {
 		fmt.Println(dest)
